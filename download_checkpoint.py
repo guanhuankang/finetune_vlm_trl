1a5fd973fb28925e96a32a21c5b4ce4076d96832
huankguan2-4090 <2659814334@qq.com>
2025-07-13 16:02:07 +0800

    requiremnt

---
+++ download_checkpoint.py
@@ -0,0 +1,5 @@
+from huggingface_hub import snapshot_download
+# model_id = "Qwen/Qwen-VL-Chat"
+model_id = "Qwen/Qwen2-VL-7B-Instruct"
+local_dir = model_id # adjust as needed
+snapshot_download(repo_id=model_id, local_dir=local_dir, local_dir_use_symlinks=False)
