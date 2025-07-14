6aa1ad72dc15b8a39e39b34de3192e142c42c717
MacOS <2659814334@qq.com>
2025-07-14 16:44:02 +0800

    debug compute_metrics

--- compute_metrics.py
+++ compute_metrics.py
@@ -3,11 +3,19 @@ import numpy as np
 
 def compute_metrics(eval_pred: EvalPrediction):
     predictions, labels = eval_pred
-    print(predictions, labels)
-    predictions = np.argmax(predictions, axis=1)
     
+    print(predictions, "$$",  labels)
+    print(type(predictions), type(labels))
+    print(type(predictions[0]), type(labels[0]), len(predictions), len(labels))
+    print(predictions.shape, len(labels), len(labels[0]))
+    
+    predictions = np.argmax(predictions, axis=1)
+    print(predictions, labels)
+
     accuracy = (predictions == labels).mean()
     
+    print("acc:", accuracy)
+
     return {
         "accuracy": accuracy,
         "custom_metric": 0.0
