4c297807b1ccd625732195da15ac0ab339251df0
MacOS <2659814334@qq.com>
2025-07-16 14:16:08 +0800

    u

--- callbacks.py
+++ callbacks.py
@@ -16,18 +16,23 @@ class GenerationEvalCallback(TrainerCallback):
         processor = kwargs["processing_class"]
         eval_dataloader = kwargs["eval_dataloader"]
 
+        trim = lambda input_ids, output_ids: [ out_ids[len(in_ids)::] for in_ids, out_ids in zip(input_ids, output_ids)]
+        
         for batch in eval_dataloader:
-            model_inputs = batch.batch_val.to(batch.device)
+            model_inputs = batch.batch_val.to(model.device)
             generated_ids = model.generate(**model_inputs, max_new_tokens=1024)
-            output_text = processor.batch_decode(
-                generated_ids,
-                skip_special_tokens=True,
-                clean_up_tokenization_spaces=False,
-            )
-            print("output_text:", output_text)
+            
+            generated_ids = trim(model_inputs.input_ids, generated_ids)
 
+            generated_text = processor.batch_decode(
+                generated_ids,
+                skip_special_tokens=True
+            )
+            print("generated_text:", generated_text)
+
+            label_ids = trim(model_inputs.input_ids, batch.labels)
             label_text = processor.batch_decode(
-                batch.input_ids,
+                label_ids,
                 skip_special_tokens=True,
                 clean_up_tokenization_spaces=False,
             )
