1a407c09abceaa8ca36e6f8e8acc236a0c553216
huankguan2-4090 <2659814334@qq.com>
2025-07-20 19:58:15 +0800

    slurm4

--- slurm4.sh
+++ slurm4.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1      # 1 MPI tasks on EACH NODE
 #SBATCH --cpus-per-task=32        # 4 OpenMP threads on EACH MPI TASK
 #SBATCH --gres=gpu:a100:4        # Using 1 GPU card
-#SBATCH --mem=1024GB               # Request 50GB memory
+#SBATCH --mem=512GB               # Request 50GB memory
 #SBATCH --time=1-06:00:00        # Time limit day-hrs:min:sec
 #SBATCH --output=log/%j/output_GPUx4.txt   # Standard output
 #SBATCH --error=log/%j/error_GPUx4.txt    # Standard error log
