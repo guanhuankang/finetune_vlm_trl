49f5cc47a0efc79053410665183bff6ca46a6a9b
huankguan2-4090 <2659814334@qq.com>
2025-07-24 11:28:43 +0800

    update slurm.sh

--- scripts/special_cs.sh
+++ scripts/special_cs.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+run_name=$(head -c2 /dev/urandom | od -An -tx1 | tr -d ' \n')-$(date +"%Y%m%d-%H%M%S")
+
+args="$@"
+
+working_dir=log/${run_name}/
+
+mkdir -p $working_dir
+
+cat > ${working_dir}/job_${run_name}.slurm <<EOL
+#!/bin/bash
 #SBATCH --partition=special_cs
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -6,11 +16,15 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=256GB
 #SBATCH --time=5-00:00:00
+#SBATCH --output=log/${run_name}.out
+#SBATCH --error=log/${run_name}.err
 
 cd /home/huankguan2/scratch/finetune_vlm_trl
-mkdir -p log
 source .venv/bin/activate
 
-run_name=$(head -c2 /dev/urandom | od -An -tx1 | tr -d ' \n')-$(date +"%Y%m%d-%H%M%S")
+python main.py --run_name ${run_name} ${args}
+EOL
 
-python main.py --run_name "$run_name" "$@" > "log/${run_name}.out" 2> "log/${run_name}.err"
+# Submit job
+cd ${working_dir}
+sbatch -A pa_cs_department job_${run_name}.slurm
